26b6d8094e79583b6f5171b32cd621598d26df71
David THIBAU <david.thibau@gmail.com>
2022-06-14 15:54:06 +0200

    WebClient

---
+++ tp6RestClient/src/test/java/org/formation/RestClientApplicationTests.java
@@ -0,0 +1,59 @@
+package org.formation;
+
+
+import org.formation.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@SpringBootTest
+class RestClientApplicationTests {
+
+	@Autowired
+	WebClient.Builder builder;
+	
+	WebClient client;
+	
+	
+	private WebTestClient webTestClient;
+	
+	@BeforeEach
+	void buildClient() {
+		client = builder.baseUrl("http://localhost:8080").build();
+	}
+	
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void getMemberById() {
+		
+		User m = client.get()
+		      .uri("/members/1")
+		      .accept(MediaType.APPLICATION_JSON)
+		      .retrieve().bodyToMono(User.class).block();
+		
+		System.out.println(m);
+		
+	}
+	
+	@Test
+	void testMemberById() {
+		
+		webTestClient = WebTestClient.bindToServer().baseUrl("http://localhost:8080").build();
+		webTestClient.get()
+		      .uri("/members/1")
+		      .accept(MediaType.APPLICATION_JSON)
+		     .exchange().expectStatus().isOk().expectBody().consumeWith(b -> {
+		    	 System.out.println(b);
+		     });
+		
+		
+	}
+	
+}
