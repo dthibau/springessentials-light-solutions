1eb7608332fd38483cbb0f8b0c8d8497828f0e7d
David THIBAU <david.thibau@gmail.com>
2022-10-20 11:10:05 +0200

    Remove deprecated WebSecurityConfigurer

---
+++ tp5_MembersDocuments/src/main/java/org/formation/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package org.formation;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/**/*.css","/**/*.js");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .authorizeRequests()
+                .requestMatchers("/home", "/", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/actuator/**")
+                .permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/**").authenticated()
+                .requestMatchers("/api/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin();
+        return http.build();
+    }
+
+}
--- tp5_MembersDocuments/src/main/java/org/formation/service/DocumentService.java
+++ tp5_MembersDocuments/src/main/java/org/formation/service/DocumentService.java
@@ -1,11 +1,11 @@
 package org.formation.service;
 
-import javax.transaction.Transactional;
 
 import org.formation.model.Document;
 import org.formation.model.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class DocumentService {
